446f92d637f6bb55aa43b994d4ce485a17e0aba7
Hiliazov, Oleksandr <ohiliazov@deloitte.com>
2019-11-29 17:32:14 +0200

    Renamed db var

--- config.py
+++ config.py
@@ -1,21 +1,21 @@
 class BaseConfig:
-    SQLALCHEMY_URL = None
+    DATABASE_URI = None
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_ENCODING = 'utf8'
     WEBSOCKET_PORT = None
 
 
 class DevConfig(BaseConfig):
-    SQLALCHEMY_URL = 'sqlite:///explorebaduk.db'
+    DATABASE_URI = 'sqlite:///explorebaduk.db'
     WEBSOCKET_PORT = 8080
 
 
 class TestConfig(BaseConfig):
-    SQLALCHEMY_URL = 'sqlite:///explorebaduk.db'
+    DATABASE_URI = 'sqlite:///explorebaduk.db'
     WEBSOCKET_PORT = 8081
 
 
 class ProdConfig(BaseConfig):
-    SQLALCHEMY_URL = 'mysql://root@localhost/explorebaduk'
+    DATABASE_URI = 'mysql://root@localhost/explorebaduk'
     SQLALCHEMY_ECHO = False
     WEBSOCKET_PORT = 8000
