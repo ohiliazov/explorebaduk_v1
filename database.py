d603154d21540cd370d64a1d0cd32eb683e6593a
Hiliazov, Oleksandr <ohiliazov@deloitte.com>
2019-11-29 17:30:42 +0200

    Create test database with users and tokens

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ from sqlalchemy import Column, String, Integer
 Base = declarative_base()
 
 
-class Users(Base):
+class User(Base):
     __tablename__ = 'users'
 
     user_id = Column(Integer, primary_key=True)
@@ -13,7 +13,7 @@ class Users(Base):
     password = Column(String(255))
 
 
-class SignInTokens(Base):
+class SignInToken(Base):
     __tablename__ = 'signin_tokens'
 
     user_id = Column(Integer, primary_key=True)
@@ -22,7 +22,23 @@ class SignInTokens(Base):
 
 if __name__ == '__main__':
     from sqlalchemy.engine import create_engine
+    from sqlalchemy.orm import sessionmaker
     from config import DevConfig
 
     engine = create_engine(DevConfig.SQLALCHEMY_URL)
     Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    session.execute('DELETE FROM users;')
+    session.execute('DELETE FROM signin_tokens;')
+
+    for i in range(100):
+        user = User(user_id=i, email=f'test{i}@test.test', password='test')
+        token = SignInToken(user_id=i, token=f'token_{i}')
+
+        session.add(user)
+        session.add(token)
+
+    session.commit()
+    session.close()
