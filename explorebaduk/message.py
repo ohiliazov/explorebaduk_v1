564b610bb71112c7565267ff20862aef26d14681
Hiliazov, Oleksandr <ohiliazov@deloitte.com>
2020-01-03 18:40:36 +0200

    Added tests on auth login messages

--- explorebaduk/message.py
+++ explorebaduk/message.py
@@ -3,13 +3,39 @@ from typing import Tuple, Dict, Any
 
 
 MESSAGE_PATTERNS = {
-    'auth': [re.compile(r"auth (?P<action>login) (?P<username>\w+) (?P<token>\w{64})"),
-             re.compile(r"auth (?P<action>logout)")],
-    'challenge': [re.compile(r"challenge (?P<action>new) (?P<game_type>\d)R(?P<ruleset>\d)P(?P<to_join>\d+)"
-                             r"U(?P<undo>\d)P(?P<pause>\d)A(?P<analyze>\d)P(?P<private>\d) "
-                             r"(?P<board_size>\d{,2}(?::\d{,2})?) "
-                             r"(?P<tyme_system>\d+)M(?P<main_time>\d+)O(?P<overtime>\d+)"
-                             r"P(?P<periods>\d+)S(?P<stones>\d+)B(?P<bonus>\d+)D(?P<delay>\d+)")],
+    "auth": [
+        re.compile(r"^auth (?P<action>login) (?P<username>\w+) (?P<token>\w{64})$"),
+        re.compile(r"^auth (?P<action>logout)$"),
+    ],
+    "challenge": [
+        re.compile(
+            r"challenge (?P<action>new) (?P<type>\d),(?P<rules>\d),(?P<players>\d+),"
+            r"(?P<board_size>\d{,2}(?::\d{,2})?) "
+            r"F(?P<open>\d)(?P<undo>\d)(?P<pause>\d) "
+            r"T(?P<tyme_system>\d+),(?P<main_time>\d+),"
+            r"(?P<overtime>\d+),(?P<periods>\d+),(?P<stones>\d+),"
+            r"(?P<bonus>\d+),(?P<delay>\d+)"
+        ),
+        re.compile(r"challenge (?P<action>cancel) (?P<challenge_id>\d+)"),
+        re.compile(r"challenge (?P<action>join) (?P<challenge_id>\d+)"),
+        re.compile(r"challenge (?P<action>leave) (?P<challenge_id>\d+)"),
+        re.compile(r"challenge (?P<action>accept) (?P<challenge_id>\d+) (?P<player_id>\d+)"),
+        re.compile(
+            r"challenge (?P<action>change) (?P<challenge_id>\d+) "
+            r"F(?P<open>\d)(?P<undo>\d)(?P<pause>\d) "
+            r"T(?P<tyme_system>\d+),(?P<main_time>\d+),"
+            r"(?P<overtime>\d+),(?P<periods>\d+),(?P<stones>\d+),"
+            r"(?P<bonus>\d+),(?P<delay>\d+)"
+        ),
+        re.compile(
+            r"challenge (?P<action>return) (?P<player_id>\d+) "
+            r"F(?P<open>\d)(?P<undo>\d)(?P<pause>\d) "
+            r"T(?P<tyme_system>\d+),(?P<main_time>\d+),"
+            r"(?P<overtime>\d+),(?P<periods>\d+),(?P<stones>\d+),"
+            r"(?P<bonus>\d+),(?P<delay>\d+)"
+        ),
+        re.compile(r"challenge (?P<action>confirm) (?P<challenge_id>\d+)"),
+    ],
 }
 
 
---
+++ tests/test_message.py
@@ -0,0 +1,33 @@
+import string
+import random
+import pytest
+from explorebaduk.message import parse_message, InvalidMessageError
+
+
+good_username = ''.join([random.choice(string.ascii_letters+string.digits) for _ in range(32)])
+good_token = ''.join([random.choice(string.ascii_letters+string.digits) for _ in range(64)])
+bad_token = good_token
+
+
+def test_auth_login_ok():
+    for i in range(100):
+        username = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(i+1)])
+        token = ''.join([random.choice(string.ascii_letters + string.digits) for _ in range(64)])
+        message_type, data = parse_message(f"auth login {username} {token}")
+
+        assert data == {'action': 'login', 'username': username, 'token': token}
+
+
+@pytest.mark.parametrize("message", [
+    f'auth login {good_username} {good_token[:-1]}',
+    f'auth login {good_username} {good_token} ',
+    f'auth login {good_username} {good_token}z',
+    f'auth login {good_username} ',
+    f'auth login {good_username}',
+    f'auth login ',
+    f'auth login',
+])
+def test_auth_login_error(message):
+
+    with pytest.raises(InvalidMessageError):
+        parse_message(message)
