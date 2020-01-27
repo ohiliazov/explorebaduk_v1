7ae1311fb1fbb1832a9ceee2bf8b8be900aceddd
Hiliazov, Oleksandr <ohiliazov@deloitte.com>
2020-01-27 18:27:11 +0200

    Added handler

--- explorebaduk/handlers/matchmaker.py
+++ explorebaduk/handlers/matchmaker.py
@@ -1,6 +1,6 @@
 import asyncio
 import logging
-from typing import List, Dict
+from typing import List, Tuple
 
 from explorebaduk.constants import AUTO_MATCH_DELAY
 from explorebaduk.server import LOBBY
@@ -9,7 +9,11 @@ logger = logging.getLogger('matchmaker')
 
 
 # TODO: implement
-async def matchmaker() -> List[Dict]:
+async def matchmaker() -> List[Tuple]:
+    """
+    Return data in format <player1> <player2> <time_settings>
+    :return:
+    """
     logger.info(f'current lobby: {LOBBY}')
     return []
 
@@ -33,3 +37,14 @@ async def auto_match_manager():
         else:
             logger.info(f'Matches found: {len(matches)}')
         await asyncio.sleep(AUTO_MATCH_DELAY)
+
+
+# TODO: implement
+async def handle_auto_match(ws, data: dict):
+    """
+    Handles auto-match message
+    :param ws:
+    :param data: contains time settings, lower and upper rating bounds
+    :return:
+    """
+    logger.info(f'Incoming data: {data}')
