7f5591fe0d5da2646271aaa2b17e47ff40b52a73
Hiliazov, Oleksandr <ohiliazov@deloitte.com>
2020-10-06 18:14:16 +0300

    added constants

---
+++ explorebaduk/constants.py
@@ -0,0 +1,4 @@
+ALLOWED_GAME_TYPES = ["ranked", "free"]
+ALLOWED_RULES = ["japanese", "chinese"]
+ALLOWED_HANDICAP_STONES = [0, 2, 3, 4, 5, 6, 7, 8, 9]
+ALLOWED_TIME_SYSTEMS = ["unlimited", "absolute", "byo-yomi", "canadian", "fischer"]
